d932adddc4e2631a21e59737ef5b63b7355a1ec6
Amit Nivedan Kalra <amitnkalra@icloud.com>
2015-07-18 12:15:18 -0700

    Initial Commit

---
+++ Spotlight API/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spotlight API
+//
+//  Created by Amit Nivedan Kalra on 7/18/15.
+//  Copyright © 2015 Amit Nivedan Kalra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
